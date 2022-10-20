c43f812eebffc8c1d795b4590212aa7959fa430f
Jamie Stewart <jastewart160@gmail.com>
2022-10-20 11:57:39 +0100

    First commit for Sakila

---
+++ SakilaFilmProj/demo/src/main/java/APIComponents/demo/SakilaFilmArtGenApplication.java
@@ -0,0 +1,13 @@
+package APIComponents.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaFilmArtGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaFilmArtGenApplication.class, args);
+	}
+
+}
---
+++ SakilaFilmProj/demo/src/test/java/APIComponents/demo/SakilaFilmArtGenApplicationTests.java
@@ -0,0 +1,13 @@
+package APIComponents.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaFilmArtGenApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
